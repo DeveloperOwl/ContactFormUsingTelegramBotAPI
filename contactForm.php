80f65efae3b0ec76a103f926d1db78a1c807299b
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-28 23:42:48 -0600

    modefied message

--- contactForm.php
+++ contactForm.php
@@ -3,7 +3,7 @@ if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"]))
 	
 
 	//combining fullname, email and message
-	$wholeMessage = "Name:".$_GET["fullname"]."\nEmail:".$_GET["email"]."\nMessage:".$_GET["message"];
+	$wholeMessage = "#Name:".$_GET["fullname"]."#Email:".$_GET["email"]."#Message:".$_GET["message"];
 	$botTokenG ="124598827:AAHY7RLm7K-dCgdB6qD-f8fBGKnV-G7z0NU";
 	echo $wholeMessage;
 	$apiUrl ="https://api.telegram.org/bot";
