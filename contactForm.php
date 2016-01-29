e25b73b4a77dc30656a10254f9ec5a1762bd1223
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-29 00:32:09 -0600

    get_file_contents

--- contactForm.php
+++ contactForm.php
@@ -8,9 +8,9 @@ if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"]))
 	echo $wholeMessage;
 	$apiUrl ="https://api.telegram.org/bot";
 
-	$response1 = http_get("https://api.telegram.org/bot199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8/sendmessage?chat_id=126445870&text=".$wholeMessage);
+	$response1 = file_get_contents("https://api.telegram.org/bot199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8/sendmessage?chat_id=126445870&text=".$wholeMessage);
 	
-	$response = http_get($apiUrl.$botTokenG."/sendmessage?chat_id=126445870&text=".$wholeMessage);
+	$response = file_get_contents($apiUrl.$botTokenG."/sendmessage?chat_id=126445870&text=".$wholeMessage);
 	
 	
 }
