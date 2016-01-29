38e031d3aee5daf828df7583218cd7153c2601d9
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-29 00:18:06 -0600

    chaned bot key

--- contactForm.php
+++ contactForm.php
@@ -4,11 +4,13 @@ if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"]))
 
 	//combining fullname, email and message
 	$wholeMessage = "#Name:".$_GET["fullname"]."#Email:".$_GET["email"]."#Message:".$_GET["message"];
-	$botTokenG ="124598827:AAHY7RLm7K-dCgdB6qD-f8fBGKnV-G7z0NU";
+	$botTokenG ="199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8";
 	echo $wholeMessage;
 	$apiUrl ="https://api.telegram.org/bot";
 
-	$response = http_get($apiUrl.$botTokenG."/sendmessage?chat_id=99797854&text=".$wholeMessage);
+	$response1 = http_get("https://api.telegram.org/bot199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8/sendmessage?chat_id=126445870&text=".$wholeMessage);
+	
+	$response = http_get($apiUrl.$botTokenG."/sendmessage?chat_id=126445870&text=".$wholeMessage);
 	
 	
 }
