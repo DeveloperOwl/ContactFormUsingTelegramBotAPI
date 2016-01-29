4e5a14a41e25c099dec6072a7c5fbef6b7843498
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-28 23:41:07 -0600

    display message

--- contactForm.php
+++ contactForm.php
@@ -5,12 +5,12 @@ if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"]))
 	//combining fullname, email and message
 	$wholeMessage = "Name:".$_GET["fullname"]."\nEmail:".$_GET["email"]."\nMessage:".$_GET["message"];
 	$botTokenG ="124598827:AAHY7RLm7K-dCgdB6qD-f8fBGKnV-G7z0NU";
-	
+	echo $wholeMessage;
 	$apiUrl ="https://api.telegram.org/bot";
 
 	$response = http_get($apiUrl.$botTokenG."/sendmessage?chat_id=99797854&text=".$wholeMessage);
 	
-	echo $response;
+	
 }
 else{
 	echo "failed!";
