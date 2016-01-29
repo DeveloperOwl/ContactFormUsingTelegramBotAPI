616d42e89f75974388f5e3d9e1d8b345c7a11d0b
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-28 23:22:37 -0600

    changed get element names

--- contactForm.php
+++ contactForm.php
@@ -1,9 +1,9 @@
 <?php
-if(isset($_GET["name"]) && isset($_GET["email"]) && isset($_GET["message"])){
+if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"])){
 	
 
-	//combining name, email and message
-	$wholeMessage = "Name:".$_GET["name"]."\nEmail:".$_GET["email"]."\nMessage:".$_GET["message"];
+	//combining fullname, email and message
+	$wholeMessage = "Name:".$_GET["fullname"]."\nEmail:".$_GET["email"]."\nMessage:".$_GET["message"];
 	$botTokenG ="124598827:AAHY7RLm7K-dCgdB6qD-f8fBGKnV-G7z0NU";
 	
 	$apiUrl ="https://api.telegram.org/bot";
