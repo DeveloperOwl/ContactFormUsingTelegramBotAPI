20ffc13018612bdaa500c424950027f9ca410e7a
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2016-01-29 01:35:48 -0600

    adding redirect to my blog from

--- contactForm.php
+++ contactForm.php
@@ -4,11 +4,8 @@ if(isset($_GET["fullname"]) && isset($_GET["email"]) && isset($_GET["message"]))
 
 	//combining fullname, email and message
 	$wholeMessage = "->Name: ".$_GET["fullname"]." ->Email: ".$_GET["email"]." ->Message: ".$_GET["message"];
-	$botTokenG ="199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8";
 	echo $wholeMessage;
-	$apiUrl ="https://api.telegram.org/bot";
-
-	$data = $apiUrl.$botTokenG"/sendmessage?chat_id=126445870&text=".$wholeMessage;
+	$data = "https://api.telegram.org/bot199479284:AAGnu34BPQr-hhDOkCGKAB2zO8a-qwcEGy8/sendmessage?chat_id=126445870&text=".$wholeMessage;
 
 	
 	$response = file_get_contents($data);
